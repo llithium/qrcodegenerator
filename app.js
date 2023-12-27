e1ecce0f92f0189bbe8faafea8876e881cef542b
llithium <barrowjr0@gmail.com>
2023-12-27 11:55:42 -0500

    implement API

--- app.js
+++ app.js
@@ -5,11 +5,31 @@ import axios from "axios";
 const app = express();
 const port = 3000;
 
+const apiURL = "https://api.qrserver.com/v1/create-qr-code/?data=";
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const qrCodeURL = req.body.qrCodeURL;
+
+    const response = await axios.get(apiURL + `/${qrCodeURL}/&size=200x200`);
+    const result = response.config.url;
+    console.log(response.config.url);
+    res.render("index.ejs", {
+      qrCode: result,
+    });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
 });
 
 app.listen(port, () => {
