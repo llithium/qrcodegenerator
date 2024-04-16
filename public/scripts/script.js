71312fd4d46ce1bdeaad75c264bcd25459f2938f
llithium <barrowjr0@gmail.com>
2024-04-16 10:57:39 -0400

    Add input autoselection

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,3 +1,9 @@
+window.onload = function () {
+  var input = document.getElementById("qrCodeInput");
+  // input.focus();
+  input.setSelectionRange(0, input.value.length);
+};
+
 let button = document.getElementById("generateButton");
 let input = document.getElementById("qrCodeInput");
 input.addEventListener("input", function () {
