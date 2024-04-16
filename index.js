603033c9d68d72d2a67081927ea6116fa978a259
llithium <barrowjr0@gmail.com>
2024-04-16 14:30:22 -0400

    Add wifi code option

--- index.js
+++ index.js
@@ -21,12 +21,34 @@ app.get("/generate", (req, res) => {
 app.post("/generate", async (req, res) => {
   try {
     const qrCodeURL = req.body.qrCodeURL;
-
-    const response = await axios.get(apiURL + `/${qrCodeURL}/&size=1000x1000`);
+    console.log(req.body);
+    const response = await axios.get(apiURL + `${qrCodeURL}&size=1000x1000`);
     const result = response.config.url;
     res.render("index.ejs", {
       qrCode: result,
       qrCodeContent: qrCodeURL,
+    });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
+});
+
+app.post("/generatewifi", async (req, res) => {
+  try {
+    const qrCodeURL = `WIFI:S:${req.body.wifiNetworkName};T:${req.body.encryption};P:${req.body.wifiPassowrd};;`;
+    console.log(req.body);
+    console.log(qrCodeURL);
+    const response = await axios.get(apiURL + `${qrCodeURL}&size=1000x1000`);
+    const result = response.config.url;
+    const returning = true;
+    res.render("index.ejs", {
+      qrCode: result,
+      netWorkName: req.body.wifiNetworkName,
+      password: req.body.wifiPassowrd,
+      returning: returning,
     });
   } catch (error) {
     console.error("Failed to make request:", error.message);
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,11 +1,44 @@
 window.onload = function () {
-  var input = document.getElementById("qrCodeInput");
+  let input = document.getElementById("qrCodeInput");
   // input.focus();
   input.setSelectionRange(0, input.value.length);
 };
 
-let button = document.getElementById("generateButton");
-let input = document.getElementById("qrCodeInput");
-input.addEventListener("input", function () {
-  button.disabled = this.value === "";
+let textGenerateButton = document.getElementById("textGenerateButton");
+let qrCodeInput = document.getElementById("qrCodeInput");
+
+qrCodeInput.addEventListener("input", function () {
+  textGenerateButton.disabled = this.value === "";
 });
+
+let wifiGenerateButton = document.getElementById("wifiGenerateButton");
+let wifiNetworkName = document.getElementById("wifiNetworkName");
+let wifiPassowrd = document.getElementById("wifiPassowrd");
+
+wifiNetworkName.addEventListener("input", function () {
+  wifiGenerateButton.disabled = this.value === "";
+});
+
+let textButton = document.getElementById("textButton");
+let wifiButton = document.getElementById("wifiButton");
+let textForm = document.getElementById("textForm");
+let wifiForm = document.getElementById("wifiForm");
+
+document
+  .getElementById("qrCodeType")
+  .addEventListener("click", function (event) {
+    switch (event.target.id) {
+      case "wifiButton":
+        textForm.classList.add("hidden");
+        wifiForm.classList.remove("hidden");
+        break;
+      case "textButton":
+        textForm.classList.remove("hidden");
+        wifiForm.classList.add("hidden");
+        break;
+
+      default:
+        break;
+    }
+    console.log(event.target.id);
+  });
